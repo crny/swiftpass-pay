aca40ee63a9bb06fa13b8765bc342f93b8712599
严洪力 <julyyan@ebudaowei.cn>
2017-09-18 17:17:19 +0800

    查询退款状态

--- src/Pay/RawQueryRefund.php
+++ src/Pay/RawQueryRefund.php
@@ -23,7 +23,7 @@ class RawQueryRefund extends WebPay
     }
 
 
-    /**预支付订单
+    /**发起退款
      * @param $playLoad array 微信支付相关参数
      * @return mixed
      * @throws \Exception
@@ -47,6 +47,42 @@ class RawQueryRefund extends WebPay
             'sign_type' => parent::SIGN_TYPE,
             'mch_id' => $playLoad['mch_id'],
             'sign_agentno' => $playLoad['sign_agentno'],
+            'out_refund_no' => $playLoad['out_refund_no'],
+            'nonce_str' => Utils::randomString()
+        ];
+
+        $metaData['sign'] = parent::Sign($metaData, $playLoad['mchKey']);
+        $response = Requests::post($this->Url,[], parent::DataSerialization($metaData));
+        return parent::DataDeserialization($response->body);
+
+    }
+
+
+    /**
+     * 查询退款情况
+     * @param $playLoad
+     *
+     * @return mixed
+     * @throws \Exception
+     */
+    public function QueryRefund($playLoad)
+    {
+        $v= new Validator();
+        $v->required('mch_id')->string()->lengthBetween(1,32);
+        $v->required('out_refund_no')->string()->lengthBetween(1,32);
+        $v->required('mchKey')->string();
+
+        $valid = $v->validate($playLoad);
+        if(!$valid->isValid()){
+            throw  new \Exception(json_encode($valid->getMessages()));
+        }
+
+        $metaData = [
+            'service' => 'unified.trade.refundquery',
+            'version' => parent::VERSION,
+            'charset' => parent::CHARSET,
+            'sign_type' => parent::SIGN_TYPE,
+            'mch_id' => $playLoad['mch_id'],
             'out_refund_no' => $playLoad['out_refund_no'],
             'nonce_str' => Utils::randomString()
         ];
